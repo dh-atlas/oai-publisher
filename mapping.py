f5a57d77d3f85242b7c13c112c238227e000b60a
Alessia <alessia.bardi@gmail.com>
2025-05-13 20:21:24 +0200

    Fixed mapping for creator list and other fields of type list

--- mapping.py
+++ mapping.py
@@ -1,39 +1,40 @@
 import xml.etree.ElementTree as ET
-import mapping_config
+
+from mapping_config import mapping_config_openaire
 
 
-def json_to_datacite(sparql_son_response, mapping):
+def json_to_datacite(json_sparql_response, mapping):
     root = ET.Element("resource", xmlns="http://datacite.org/schema/kernel-4")
 
-    for binding in sparql_son_response["results"]["bindings"]:
+    for binding in json_sparql_response["results"]["bindings"]:
         for key, config in mapping.items():
             json_field = config["json_field"]
             xml_tag = config["xml_tag"]
             attributes = config.get("attributes", {})
-            split = config.get("split")
+            split = config.get("split", None)
             year_only = config.get("year_only", False)
 
             if json_field in binding:
                 value = binding[json_field]["value"]
 
                 if split:
-                    values = value.split(split)
+                    parent_element = ET.SubElement(root, xml_tag)
+                    for item in value.split(split):
+                        child_element = ET.SubElement(parent_element, key)
+                        child_element.text = item
                 else:
-                    values = [value]
-
-                for val in values:
                     element = ET.SubElement(root, xml_tag, **attributes)
                     if year_only:
-                        val = val.split("-")[0]
-                    element.text = val
+                        element.text = value.split("-")[0]
+                    else:
+                        element.text = value
 
     return ET.tostring(root, encoding="unicode")
 
-
 # Sample JSON response
-json_response = {
-    "head": {
-        "vars": [
+json_sample = {
+    "head" : {
+        "vars" : [
             "dataset",
             "type",
             "names",
@@ -51,78 +52,69 @@ json_response = {
             "conditionsOfAccess"
         ]
     },
-    "results": {
-        "bindings": [
+    "results" : {
+        "bindings" : [
             {
-                "dataset": {
-                    "type": "uri",
-                    "value": "https://w3id.org/dh-atlas/1728943937-3007112"
+                "dataset" : {
+                    "type" : "uri",
+                    "value" : "https://w3id.org/dh-atlas/1728943937-3007112"
                 },
-                "type": {
-                    "type": "uri",
-                    "value": "https://schema.org/Dataset"
+                "type" : {
+                    "type" : "uri",
+                    "value" : "https://schema.org/Dataset"
                 },
-                "names": {
-                    "type": "literal",
-                    "value": "Zeri Photo Archive RDF Dataset"
+                "names" : {
+                    "type" : "literal",
+                    "value" : "Zeri Photo Archive RDF Dataset"
                 },
-                "descriptions": {
-                    "type": "literal",
-                    "value": "The Zeri Photo Archive RDF dataset represents a considerable subset of data already "
-                             "available at Zeri Catalog web site and discoverable through the Europeana Portal, "
-                             "mostly regarding works of Modern Art (15th-16th centuries): about 19.000 works of art "
-                             "and more than 30.000 photographs depicting such works are accurately described by means "
-                             "of like 10 million of RDF triples."
+                "descriptions" : {
+                    "type" : "literal",
+                    "value" : "The Zeri Photo Archive RDF dataset represents a considerable subset of data already available at Zeri Catalog web site and discoverable through the Europeana Portal, mostly regarding works of Modern Art (15th-16th centuries): about 19.000 works of art and more than 30.000 photographs depicting such works are accurately described by means of like 10 million of RDF triples."
                 },
-                "creators": {
-                    "type": "literal",
-                    "value": "https://w3id.org/dh-atlas/1728943969-854 || https://w3id.org/dh-atlas/1728943988-693 || "
-                             "https://w3id.org/dh-atlas/1728944010-053 || https://w3id.org/dh-atlas/1728944026-501 || "
-                             "https://w3id.org/dh-atlas/1728944045-966"
+                "creators" : {
+                    "type" : "literal",
+                    "value" : "https://w3id.org/dh-atlas/1728943969-854 || https://w3id.org/dh-atlas/1728943988-693 || https://w3id.org/dh-atlas/1728944010-053 || https://w3id.org/dh-atlas/1728944026-501 || https://w3id.org/dh-atlas/1728944045-966"
                 },
-                "contributors": {
-                    "type": "literal",
-                    "value": ""
+                "contributors" : {
+                    "type" : "literal",
+                    "value" : ""
                 },
-                "publishers": {
-                    "type": "literal",
-                    "value": "https://w3id.org/dh-atlas/1728944565-1916842"
+                "publishers" : {
+                    "type" : "literal",
+                    "value" : "https://w3id.org/dh-atlas/1728944565-1916842"
                 },
-                "languages": {
-                    "type": "literal",
-                    "value": "http://publications.europa.eu/resource/authority/language/ENG || "
-                             "http://publications.europa.eu/resource/authority/language/ITA"
+                "languages" : {
+                    "type" : "literal",
+                    "value" : "http://publications.europa.eu/resource/authority/language/ENG || http://publications.europa.eu/resource/authority/language/ITA"
                 },
-                "educationalUses": {
-                    "type": "literal",
-                    "value": "https://vocabs.dariah.eu/tadirah/cataloging || "
-                             "https://vocabs.dariah.eu/tadirah/knowledgeDiscovery || "
-                             "https://vocabs.dariah.eu/tadirah/knowledgeExtraction"
+                "educationalUses" : {
+                    "type" : "literal",
+                    "value" : "https://vocabs.dariah.eu/tadirah/cataloging || https://vocabs.dariah.eu/tadirah/knowledgeDiscovery || https://vocabs.dariah.eu/tadirah/knowledgeExtraction"
                 },
-                "distributions": {
-                    "type": "literal",
-                    "value": "https://amsacta.unibo.it/id/eprint/5497/2/dump-complete-13-1-2017.zip"
+                "distributions" : {
+                    "type" : "literal",
+                    "value" : "https://amsacta.unibo.it/id/eprint/5497/2/dump-complete-13-1-2017.zip"
                 },
-                "urls": {
-                    "type": "literal",
-                    "value": "https://w3id.org/zericatalog"
+                "urls" : {
+                    "type" : "literal",
+                    "value" : "https://w3id.org/zericatalog"
                 },
-                "producers": {
-                    "type": "literal",
-                    "value": "https://w3id.org/dh-atlas/1728984604-1499374"
+                "producers" : {
+                    "type" : "literal",
+                    "value" : "https://w3id.org/dh-atlas/1728984604-1499374"
                 },
-                "additionalType": {
-                    "type": "uri",
-                    "value": "https://w3id.org/dh-atlas/LinkedOpenData"
+                "additionalType" : {
+                    "type" : "uri",
+                    "value" : "https://w3id.org/dh-atlas/LinkedOpenData"
                 },
-                "datePublished": {
-                    "datatype": "http://www.w3.org/2001/XMLSchema#date",
-                    "type": "literal",
-                    "value": "2016-05-20"
+                "datePublished" : {
+                    "datatype" : "http://www.w3.org/2001/XMLSchema#date",
+                    "type" : "literal",
+                    "value" : "2016-05-20"
                 },
-                "conditionsOfAccess": {
-                    "type": "uri",
-                    "value": "http://purl.org/coar/access_right/c_abf2"
+                "conditionsOfAccess" : {
+                    "type" : "uri",
+                    "value" : "http://purl.org/coar/access_right/c_abf2"
                 }
             }
         ]
@@ -130,10 +122,11 @@ json_response = {
 }
 
 # Convert JSON to DataCite XML
-xml_output = json_to_datacite(json_response, mapping_config.mapping_config_openaire)
+xml_output = json_to_datacite(json_sample, mapping_config_openaire)
 
-# Save the XML output to a file
-with open("datacite_record.xml", "w") as xml_file:
-    xml_file.write(xml_output)
+# Save XML to file
+with open("datacite_record.xml", "w") as f:
+    f.write(xml_output)
 
 print("DataCite XML record has been saved to datacite_record.xml")
+
--- mapping_config.py
+++ mapping_config.py
@@ -1,12 +1,12 @@
 
 mapping_config_openaire = {
-"identifier": {"json_field": "dataset", "xml_tag": "identifier", "attributes": {"identifierType": "URL"}},
-"creators": {"json_field": "creators", "xml_tag": "creators", "split": " || "},
-"title": {"json_field": "names", "xml_tag": "titles"},
-"publisher": {"json_field": "publishers", "xml_tag": "publisher"},
-"publicationYear": {"json_field": "datePublished", "xml_tag": "publicationYear", "year_only": True},
-"description": {"json_field": "descriptions", "xml_tag": "descriptions", "attributes": {"descriptionType": "Abstract"}},
-"subjects": {"json_field": "educationalUses", "xml_tag": "subjects", "split": " || "},
-"language": {"json_field": "languages", "xml_tag": "language"},
-"resourceType": {"json_field": "type", "xml_tag": "resourceType", "attributes": {"resourceTypeGeneral": "Dataset"}}
+    "identifier": {"json_field": "dataset", "xml_tag": "identifier", "attributes": {"identifierType": "URL"}},
+    "creator": {"json_field": "creators", "xml_tag": "creators", "split": " || "},
+    "title": {"json_field": "names", "xml_tag": "titles", "split": " || "},
+    "publisher": {"json_field": "publishers", "xml_tag": "publishers", "split": " || "},
+    "publicationYear": {"json_field": "datePublished", "xml_tag": "publicationYear", "year_only": True},
+    "description": {"json_field": "descriptions", "xml_tag": "descriptions", "attributes": {"descriptionType": "Abstract"}, "split": " || "},
+    "subject": {"json_field": "educationalUses", "xml_tag": "subjects", "split": " || "},
+    "language": {"json_field": "languages", "xml_tag": "languages", "split": " || "},
+    "resourceType": {"json_field": "type", "xml_tag": "resourceType", "attributes": {"resourceTypeGeneral": "Dataset"}}
 }
